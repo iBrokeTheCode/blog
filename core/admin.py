e3f5ec1e5c02b26ef78d4d54c714ea5ed38d2883
alex-brokethecode <alexrtc.dev@gmail.com>
2025-03-31 10:14:07 -0500

    chore: Create Post, Tag and Category models. Configure admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Post, Tag
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'active', 'created')
+    readonly_fields = ('created', 'updated')
+    ordering = ('name',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'active', 'created')
+    readonly_fields = ('created', 'updated')
+    ordering = ('name',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published', 'created',
+                    'author', 'category', 'post_tags')  # author, tags
+    readonly_fields = ('created', 'updated')
+    ordering = ('title', '-created', 'author')
+    search_fields = ('title', 'content', 'author__username', 'category__name')
+    list_filter = ('author', 'category', 'tags')
+
+    def post_tags(self, obj):
+        return ', '.join([tag.name for tag in obj.tags.all()])
+
+    post_tags.short_description = 'Tags'  # type: ignore
--- core/models.py
+++ core/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField(upload_to='posts/images', null=True, blank=True)
+    published = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE,
+        related_name='posts')  # Default: category.post_set
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name='posts')  # Default: author.post_set
+    tags = models.ManyToManyField(
+        Tag,
+        related_name='posts')  # Default: set.post_set
+
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+        ordering = ('-created', 'title')
+
+    def __str__(self):
+        return self.title
