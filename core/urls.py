c6ac241e3509360437c2b3c2ba8c8a695f8f273c
alex-brokethecode <alexrtc.dev@gmail.com>
2025-03-30 21:49:12 -0500

    chore: Customize styles and modularize components/templates

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,8 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('post/', views.post, name='post'),
+    path('author/', views.author, name='author'),
+    path('category/', views.category, name='category'),
+    path('date/', views.date, name='date'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,4 +2,20 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'core/index.html', {})
+    return render(request, 'core/home.html', {})
+
+
+def post(request):
+    return render(request, 'core/post.html', {})
+
+
+def author(request):
+    return render(request, 'core/home.html', {})
+
+
+def category(request):
+    return render(request, 'core/home.html', {})
+
+
+def date(request):
+    return render(request, 'core/home.html', {})
